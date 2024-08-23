e2e7a3658183cb2465743f0990bebe2c112baea6
Daniil Chibitock <daniil.chibitock@yandex.ru>
2024-08-23 16:36:08 +0300

    delete pillow

--- storage/models.py
+++ storage/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Captcha(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.FileField(upload_to='images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
